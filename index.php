77f5405b6d7dc086348661f93315e785f92830ef
jcigama <jcigama77@yahoo.com>
2021-01-15 15:32:29 -0500

    Committing functions file

--- index.php
+++ index.php
@@ -1,22 +1,19 @@
 <?php
+// Joseph Igama, Dana Clemmer
+// Date: 01/15/2021
+// File URL: http://jigama.greenriverdev.com/pp2/index.php
+
 
 //Turn on error reporting
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
-//Require the autoload file
-require_once('vendor/autoload.php');
+include("functions.php");
 
-//Create an instance of the Base class
-$f3 = Base::instance();
-$f3 = new Base();
-$f3->set('DEBUG', 3);
+echo "<h1>PHP Array Practice</h1>";
 
-//Define a default route (home page)
-$f3->route('GET /', function() {
-    $view = new Template();
-    echo $view->render('views/home.html');
-});
+$numbers = array(7, 9, 8, 9, 8, 8, 6);
 
-//Run fat free
-$f3->run();
+printArr($numbers);
+
+largest($numbers);
